f45b037f72e507444269054a4d41688c934e3944
Jiexi <273689045@qq.com>
2022-01-01 23:26:56 +0100

    20220101

--- python_test/test.py
+++ python_test/test.py
@@ -1,6 +1,11 @@
-def hello():
-    print('hello world')
+import time
+import datetime
+#from datetime import datetime
+print('现在时间')
 
-temp= hello()
+localtime_structTime=time.localtime()
+localtime_str=time.strftime("%Y-%m-%d",localtime_structTime)
+localtime_dateTime=datetime.datetime.strptime(localtime_str,"%Y-%m-%d")
 
-print(temp)
+print(type(localtime_dateTime))
+print(localtime_dateTime)
